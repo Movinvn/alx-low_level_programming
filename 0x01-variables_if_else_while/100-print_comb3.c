7dd6af248cf0a708345c4dcc55600bccb4808ad3
Movinvn <movinowino12@gmail.com>
2022-09-10 02:02:41 -0700

    m

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,59 @@
-#include <stdio.h> 
-  
- /** 
-  *   * main - main  prints all possible different combinations of two digits 
-  *    * Return: ALways 0 (Success) 
-  */ 
- int main(void) 
-{
-int n, m; 
-		    
-for (n = 48; n <= 56; n++) 
-{ 
-for (m = 49; m <= 57; m++) 
-{ 
-if (m > n) 
-{ 
-putchar(n); 
-putchar(m); 
-if (n != 56 || m != 57) 										 
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints all possible different combinations of two digits
+ *
+ *   * Return: ALways 0 (Success)
+ */
+
+int main(void)
 
 {
-putchar(','); 
-putchar(' '); 
+
+int n, m;
+
+
+
+for (n = 48; n <= 56; n++)
+
+{
+
+for (m = 49; m <= 57; m++)
+
+{
+
+if (m > n)
+
+{
+
+																					putchar(n);
+
+																					putchar(m);
+
+																					if (n != 56 || m != 57)
+
+																				       {
+
+																																											putchar(',');
+
+																																										
+																																											putchar(' ');
+
+																				
+
+																						}
+
+																					
 }
-		  									       
-} 
-} 
-putchar('\n'); 
-return (0);
+
+}
+
+}
+
+putchar('\n');
+
+return (0);
 }
